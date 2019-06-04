ff31bd61bd807c7a672cdabf4c18c630327900a9
Sean Giddings <seandgiddings@gmail.com>
2019-06-04 12:58:05 -0400

    added swapAlignment prop and set it to reverse the content being displayed on the composer section

--- src/FeaturedSection.js
+++ src/FeaturedSection.js
@@ -9,16 +9,17 @@ class FeaturedSection extends Component {
   }
 
   render() {
-    const {title} = this.props;
-    
+    const {title, description, swapAlignment} = this.props;
+    const alignment = `FeaturedSection ${swapAlignment ? 'FeaturedSection--alignRight' : 'FeaturedSection--alignLeft'}`;
+   
     return (
-      <div className="FeaturedSection">
+      <div className={alignment}>
         <div className="FeaturedSection__text">
           <h2 className='FeaturedSection__title'>
             {title}
           </h2>
           <p className='FeaturedSection__description'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            {description}
           </p>
         </div>
         <div className='FeaturedSection__images'>

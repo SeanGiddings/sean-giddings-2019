0fd777accf2ab2b24783a41639bd464ed327aedf
Sean Giddings <seandgiddings@gmail.com>
2019-06-04 12:58:46 -0400

    added button and began the work to set the button to scroll down.  no styles have been set on the button

--- src/PageHeader.js
+++ src/PageHeader.js
@@ -1,31 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PortfolioHeadshot from './Portfolio-Headshot.png';
 import './PageHeader.scss';
+import animateScrollTo from 'animated-scroll-to';
 
-function PageHeader() {
-  return (
-    <div className="PageHeader">
-      <header className="PageHeader__header">
-        <img src={PortfolioHeadshot} className="PageHeader__headshot" alt="headshot" />
-        <h1 className="PageHeader__intro">
-          Hello! I'm <span className="PageHeader__name">Sean Giddings</span> and I am
-        </h1>
-        <div className="PageHeader__professions">
-          <div className="PageHeader__profession PageHeader__profession--developer">
-            a Web Developer
+class PageHeader extends Component {
+
+  render() {
+    return (
+      <div className="PageHeader">
+        <header className="PageHeader__header">
+          <img src={PortfolioHeadshot} className="PageHeader__headshot" alt="headshot" />
+          <h1 className="PageHeader__intro">
+            Hello! I'm <span className="PageHeader__name">Sean Giddings</span> and I am
+          </h1>
+          <div className="PageHeader__professions">
+            <div className="PageHeader__profession PageHeader__profession--developer">
+              a Web Developer
+            </div>
+            <div className="PageHeader__profession PageHeader__profession--composer">
+              a Composer
+            </div>
+            <div className="PageHeader__profession PageHeader__profession--songwriter">
+              a Songwriter
+            </div>  
           </div>
-          <div className="PageHeader__profession PageHeader__profession--composer">
-            a Composer
-          </div>
-          <div className="PageHeader__profession PageHeader__profession--songwriter">
-            a Songwriter
-          </div>  
-        </div>
-      </header>
-    </div>
-  
-  
-  );
+          <button className='PageHeader__button'></button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default PageHeader;

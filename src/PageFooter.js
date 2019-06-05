d1cce3564d11eed51f7ab65c93ba905ee4329e23
Sean Giddings <seandgiddings@gmail.com>
2019-06-05 14:49:00 -0400

    created PageFooter component

---
+++ src/PageFooter.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './PageFooter.scss';
+
+class PageFooter extends Component {
+
+  render() {
+    return (
+      <div className="PageFooter">
+
+      </div>
+    );
+  }
+}
+
+export default PageFooter;

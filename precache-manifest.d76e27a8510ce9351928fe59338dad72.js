c99d9e297768a3ffc82043e4bd11793cca4caa82
seangiddings <seandgiddings@gmail.com>
2020-06-30 23:05:31 -0400

    Revert "Updates"
    This reverts commit ae0c75c4fdbb0a3f6cddc728a5522cdc940030a1.

---
+++ precache-manifest.d76e27a8510ce9351928fe59338dad72.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5871d8a75bcf5a2b2863bdbba7d91988",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57f5931a21bd0e8aa856",
+    "url": "/static/css/main.f426b1e3.chunk.css"
+  },
+  {
+    "revision": "d6afd3a7a58eeffc922a",
+    "url": "/static/js/2.47e5b218.chunk.js"
+  },
+  {
+    "revision": "57f5931a21bd0e8aa856",
+    "url": "/static/js/main.07ac5003.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d04576ba361f93f976f6fc921221f7a0",
+    "url": "/static/media/Colyer.d04576ba.png"
+  },
+  {
+    "revision": "4cbc9e1cf9d7b3562c25bfce80599164",
+    "url": "/static/media/Morning-Greys.4cbc9e1c.jpg"
+  },
+  {
+    "revision": "4a602ac92cabe813b89e211c38973c1c",
+    "url": "/static/media/Portfolio-Headshot.4a602ac9.png"
+  },
+  {
+    "revision": "58ccb9233e6381e0acaf4390bd183bb7",
+    "url": "/static/media/Poster-Collage.58ccb923.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sean-giddings-2019/precache-manifest.4ba47fde25de6e65a75ab4ecf0ebf2d4.js"
+  "/precache-manifest.d76e27a8510ce9351928fe59338dad72.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sean-giddings-2019/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });

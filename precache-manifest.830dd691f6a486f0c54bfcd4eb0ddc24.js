255192923b385b2b44ce008547a5d832df05aab0
seangiddings <seandgiddings@gmail.com>
2020-10-26 11:52:32 -0400

    Updates

---
+++ precache-manifest.830dd691f6a486f0c54bfcd4eb0ddc24.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3167d99701b96c5d2cac754178616b85",
+    "url": "/sean-giddings-2019/index.html"
+  },
+  {
+    "revision": "980a97a027f247c90157",
+    "url": "/sean-giddings-2019/static/css/main.6a2fb052.chunk.css"
+  },
+  {
+    "revision": "0772a0dac068891bf2bf",
+    "url": "/sean-giddings-2019/static/js/2.c11eeea7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sean-giddings-2019/static/js/2.c11eeea7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "980a97a027f247c90157",
+    "url": "/sean-giddings-2019/static/js/main.4e107764.chunk.js"
+  },
+  {
+    "revision": "2e6c2033100c0e7cdaeb",
+    "url": "/sean-giddings-2019/static/js/runtime-main.8e542919.js"
+  },
+  {
+    "revision": "4cbc9e1cf9d7b3562c25bfce80599164",
+    "url": "/sean-giddings-2019/static/media/Morning-Greys.4cbc9e1c.jpg"
+  },
+  {
+    "revision": "4a602ac92cabe813b89e211c38973c1c",
+    "url": "/sean-giddings-2019/static/media/Portfolio-Headshot.4a602ac9.png"
+  },
+  {
+    "revision": "58ccb9233e6381e0acaf4390bd183bb7",
+    "url": "/sean-giddings-2019/static/media/Poster-Collage.58ccb923.jpg"
+  },
+  {
+    "revision": "a0561fdfd5134be6c121bf47c1f5df32",
+    "url": "/sean-giddings-2019/static/media/castle-escape.a0561fdf.gif"
+  },
+  {
+    "revision": "86c7034ea1b598d1bcd913b80e43273d",
+    "url": "/sean-giddings-2019/static/media/webdev.86c7034e.jpg"
+  }
+]);

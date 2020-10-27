5bdb2272d77c0c82bfba97b6051d8df873f7b006
seangiddings <seandgiddings@gmail.com>
2020-10-27 16:07:03 -0400

    added github link to footer and made date programatically update

--- src/PageFooter.js
+++ src/PageFooter.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react';
 import './PageFooter.scss';
 import Linkedin from './Linkedin.png'
-
+import Github from './github.png'
 class PageFooter extends Component {
-
+  getYear() {
+    return new Date().getFullYear();
+  }
+  
   render() {
     return (
       <div className="PageFooter">
         <div className="PageFooter__text">
-          Sean Giddings <span className="PageFooter__copyright">&copy;2020</span>
+          Sean Giddings <span className="PageFooter__copyright">&copy;{this.getYear()}</span>
         </div>
-        <a href='https://www.linkedin.com/in/sean-giddings-350a3213/'>
-          <img src={Linkedin} alt="LinkedIn Icon" className='PageFooter__linkedin'></img>
-        </a>
+        <div class="PageFooter__socialmedia">
+          <a href='https://www.linkedin.com/in/sean-giddings-350a3213/' target="_blank">
+            <img src={Linkedin} alt="LinkedIn Icon" className='PageFooter__linkedin'></img>
+          </a>
+          <a href='https://github.com/SeanGiddings' target="_blank">
+            <img src={Github} alt="Github Icon" className='PageFooter__github'></img>
+          </a>
+        </div>
       </div>
     );
   }
 }
+
 
 export default PageFooter;

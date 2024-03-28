1bce094dcc89279b2e39a053a55c23244f64ac15
Sean Giddings <sean@Seans-Mac-mini.local>
2024-03-28 14:56:22 -0400

    Updates

---
+++ precache-manifest.db4c791b4ca92b6e02d7a51352e9e099.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7983bbc16128e55ffd04d1f137b97703",
+    "url": "/sean-giddings-2019/index.html"
+  },
+  {
+    "revision": "3c8f64af7b7dd3523db7",
+    "url": "/sean-giddings-2019/static/css/main.4d7689c9.chunk.css"
+  },
+  {
+    "revision": "af4b331cd02e1587f8a8",
+    "url": "/sean-giddings-2019/static/js/2.ad2772fe.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/sean-giddings-2019/static/js/2.ad2772fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c8f64af7b7dd3523db7",
+    "url": "/sean-giddings-2019/static/js/main.ae260cf0.chunk.js"
+  },
+  {
+    "revision": "bf3dd3d8d92a2237134a",
+    "url": "/sean-giddings-2019/static/js/runtime-main.67d9d74b.js"
+  },
+  {
+    "revision": "4a602ac92cabe813b89e211c38973c1c",
+    "url": "/sean-giddings-2019/static/media/Portfolio-Headshot.4a602ac9.png"
+  },
+  {
+    "revision": "58ccb9233e6381e0acaf4390bd183bb7",
+    "url": "/sean-giddings-2019/static/media/Poster-Collage.58ccb923.jpg"
+  },
+  {
+    "revision": "26f5e2b814facb5dcda1891386b7f969",
+    "url": "/sean-giddings-2019/static/media/automation.26f5e2b8.png"
+  },
+  {
+    "revision": "a0561fdfd5134be6c121bf47c1f5df32",
+    "url": "/sean-giddings-2019/static/media/castle-escape.a0561fdf.gif"
+  },
+  {
+    "revision": "9e496dbd48ca39b99ad51d4299b69244",
+    "url": "/sean-giddings-2019/static/media/github.9e496dbd.png"
+  },
+  {
+    "revision": "892b5742bf960f99a768b79b0e07f91b",
+    "url": "/sean-giddings-2019/static/media/sparrowghost.892b5742.jpeg"
+  },
+  {
+    "revision": "86c7034ea1b598d1bcd913b80e43273d",
+    "url": "/sean-giddings-2019/static/media/webdev.86c7034e.jpg"
+  }
+]);

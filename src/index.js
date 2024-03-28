617a233d2b5206a8a2000aeae5f70775c01843fd
Sean Giddings <sean@Seans-Mac-mini.local>
2024-03-28 17:07:11 -0400

    ready to deploy with command

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import LandingPage from './LandingPage';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import LandingPage from "./LandingPage";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<LandingPage />, document.getElementById('root'));
+ReactDOM.render(<LandingPage />, document.getElementById("root"));
 
 // If you want your LandingPage to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
